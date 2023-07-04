802fa67ace535adc0d04f393dabad28bf4d24401
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-04 01:49:24 -0600

    added form validators

--- sixth_project/basicapp/forms.py
+++ sixth_project/basicapp/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 
 class FormName(forms.Form):
+
     name = forms.CharField()
+
     email = forms.EmailField()
+
+    verify_email = forms.EmailField(label='Enter your email again')
+
     text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        email = cleaned_data.get("email")
+
+        vmail = cleaned_data.get("verify_email")
+
+        if email != vmail:
+            raise ValidationError("Emails are not the same")
